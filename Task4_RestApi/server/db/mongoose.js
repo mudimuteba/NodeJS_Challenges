9ffa2e6976f68a99c4125fb2b37b7477af463411
mudimuteba <mudi.muteba@gmail.com>
2019-07-09 15:40:34 +0200

    named db to Prospects

--- Task4_RestApi/server/db/mongoose.js
+++ Task4_RestApi/server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://127.0.0.1:27017/TodoApp", { useNewUrlParser: true });
+mongoose.connect("mongodb://127.0.0.1:27017/Prospects", { useNewUrlParser: true });
 module.exports = {mongoose};
